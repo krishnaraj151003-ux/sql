e5b2d001f239ec77068194b7cbff1baeea594228
krishnaraj151003-ux <krishnaraj151003@gmail.com>
2025-11-11 21:35:06 +0530

    Add files via upload

---
+++ ddl.sql
@@ -0,0 +1,15 @@
+CREATE database schooldb;
+USE schooldb;
+show databases;
+create table inform(
+stu_id  int  primary key,
+stu_name varchar(40),
+age int,
+gender  varchar(20)
+);
+
+DESC inform;
+
+
+ 
+
